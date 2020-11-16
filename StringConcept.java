1c339bd2514e91d6c0abc7439f2f5d95906cda1c
srinivastesting <koppineedi.venkatasrinivas@gmail.com>
2020-11-16 11:45:41 +0530

    first commit

---
+++ StringConcept.java
@@ -0,0 +1,16 @@
+package com.abstractmethod.practise;
+
+public class StringConcept {
+
+	public static void main(String[] args) {
+		String s1="Srinivas";
+		String s2="Srinivas";
+		System.out.println(s1);
+		System.out.println(s2);
+		s2="hello";
+		System.out.println(s1);
+		System.out.println(s2);
+
+	}
+
+}
