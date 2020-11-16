db0ee3d2996d0c9c4eb7c781876e289e6d18f055
srinivastesting <74200062+srinivastesting@users.noreply.github.com>
2020-11-16 12:13:55 +0530

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,11 +1,13 @@
 package com.abstractmethod.practise;
 
 public abstract class Bank {
+	//loan method
 	public abstract void loan();
-
+        //credit method
 	public void credit() {
 		System.out.println("credit method");
 	}
+	//debit method
 
 	public void debit() {
 		System.out.println("debit method");
